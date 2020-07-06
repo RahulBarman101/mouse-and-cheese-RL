69e5aea3d84577a92437622d476ca6a416113f90
Rahul Barman <rahulbarman1215@gmail.com>
2020-07-06 10:04:05 +0530

    added the sleep time to show the steps

--- play.py
+++ play.py
@@ -1,5 +1,6 @@
 import numpy as np
 from mouse import mouse
+import time
 
 m = mouse()
 steps = 100
@@ -10,9 +11,11 @@ directions = ['right','left','up','down']
 statex,statey = m.reset()
 for i in range(steps):
 	action = np.argmax(q_table[statex,statey])
-	print(directions[action])
+	
 	m.render()
+	time.sleep(2)
 	new_statex, new_statey, reward, done = m.step(action)
+	print(directions[action])
 	if done:
 		break
 	statex,statey = new_statex,new_statey
